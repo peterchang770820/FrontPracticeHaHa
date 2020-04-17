fca6463797a1029055ed46c7b572f1da92228989
張展翔Peter <peter@miniasp.com>
2020-04-18 01:12:12 +0800

    Vue.js 快走, p.8 的範例。

---
+++ vue-practice/src/components/url-path.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="url-path">
+    <p v-if="path === '/'">
+      You are on the home page
+    </p>
+    <p v-else>You're on {{ path }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "url-path",
+  data() {
+    return {
+      path: this.$route.path
+    };
+  },
+};
+</script>
+
+<style scoped></style>
