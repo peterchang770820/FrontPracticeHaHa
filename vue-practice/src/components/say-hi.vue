e3883a2137cb6ed40ba693e94746143ceb3c7815
張展翔Peter <peter@miniasp.com>
2020-04-17 16:26:55 +0800

    Vue.js 快跑，p.7 say-hi.vue 改寫 主要是把判斷的邏輯抽到 Template 中。 把 hour 放到 data() 中。

--- vue-practice/src/components/say-hi.vue
+++ vue-practice/src/components/say-hi.vue
@@ -2,15 +2,15 @@
   <div class="say-hi" :title="message" @click="myalert">
     <div>{{ text }}</div>
     <div>
-      <p v-if="isMorning">Good Morning!</p>
-      <p v-if="isAfternoon">Good Afternoon!</p>
-      <p v-if="isEvening">Good Evening!</p>
+      <p v-if="hours < 12">Good Morning!</p>
+      <p v-if="hours >= 12 && hours < 18">Good Afternoon!</p>
+      <p v-if="hours >= 18">Good Evening!</p>
     </div>
   </div>
 </template>
 
 <script>
-var hours = new Date().getHours();
+
 export default {
   name: "say-hi",
   props: ["message"],
@@ -18,9 +18,7 @@ export default {
   },
   data() {
     return {
-      isMorning: hours < 12,
-      isAfternoon: hours >= 12 && hours < 18,
-      isEvening: hours >= 18
+      hours: new Date().getHours()
     };
   },
   methods: {
