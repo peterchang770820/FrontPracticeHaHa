22e21c414ae5003249d48ff4c18fad1f204ac357
張展翔Peter <peter@miniasp.com>
2020-04-17 16:22:21 +0800

    Vue.js 快跑，p.6 範例。

---
+++ vue-practice/src/components/say-hi.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="say-hi" :title="message" @click="myalert">
+    <div>{{ text }}</div>
+    <div>
+      <p v-if="isMorning">Good Morning!</p>
+      <p v-if="isAfternoon">Good Afternoon!</p>
+      <p v-if="isEvening">Good Evening!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+var hours = new Date().getHours();
+export default {
+  name: "say-hi",
+  props: ["message"],
+  components: {
+  },
+  data() {
+    return {
+      isMorning: hours < 12,
+      isAfternoon: hours >= 12 && hours < 18,
+      isEvening: hours >= 18
+    };
+  },
+  methods: {
+    myalert() {
+      alert(this.message);
+    }
+  },
+  created() {
+    this.text += ", click me!";
+  }
+};
+</script>
+
+<style scoped>
+.say-hi {
+  font-weight: bold;
+  color: #41b883;
+}
+</style>
--- vue-practice/src/router/router.js
+++ vue-practice/src/router/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Home from "../components/home";
 import About from "../components/about";
 import Sample1 from "../components/js-item-sample";
+import Sample2 from "../components/say-hi";
 
 Vue.use(VueRouter);
 
@@ -22,6 +23,11 @@ const routes = [
         path: '/sample1',
         name: 'Sample1',
         component: Sample1
+    },
+    {
+        path: '/sample2',
+        name: 'Sample2',
+        component: Sample2
     }
 ];
 
