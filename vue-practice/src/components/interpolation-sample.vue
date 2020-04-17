db46e8e9dfaba539d223c1cac647dae3d42252c3
張展翔Peter <peter@miniasp.com>
2020-04-18 00:27:00 +0800

    Vue.js p.8 範例 interpolation 插值

---
+++ vue-practice/src/components/interpolation-sample.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="interpolation-sample" :title="message" @click="myalert">
+    <p>Hello, {{ greetee }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "interpolation-sample",
+  props: ["message"],
+  components: {
+  },
+  data() {
+    return {
+      greetee: 'world',
+      text: "interpolation-sample component"
+    };
+  },
+  methods: {
+    myalert() {
+      alert(this.message);
+    }
+  },
+  created() {
+    this.text += ", click me!";
+  }
+};
+</script>
+
+<style scoped>
+.interpolation-sample {
+  font-weight: bold;
+  color: #41b883;
+}
+</style>
